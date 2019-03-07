eb7cd3291c05397f739481fac50f49a7068c1379
matt-kendall <mattk91@gmail.com>
2019-03-06 23:07:29 -0500

    Add an Airport model class

---
+++ Model/Airport.cs
@@ -0,0 +1,36 @@
+﻿namespace FlightPlanner.Model
+{
+    /// <summary>
+    /// Data relating to an airport facility.
+    /// </summary>
+    public class Airport
+    {
+        /// <summary>
+        /// The location of the airport
+        /// </summary>
+        public LatLon Location { get; private set; }
+
+        /// <summary>
+        /// The name of the airport facility.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// The four-letter ICAO code of the airport facility.
+        /// </summary>
+        public string ICAOCode { get; private set; }
+
+        /// <summary>
+        /// Create a new Airport instance.
+        /// </summary>
+        /// <param name="location">The location of the airport</param>
+        /// <param name="name">The airport's name</param>
+        /// <param name="icaoCode">The airport's ICAO coe</param>
+        public Airport(LatLon location, string name, string icaoCode)
+        {
+            this.Location = location;
+            this.Name = name;
+            this.ICAOCode = icaoCode;
+        }
+    }
+}
